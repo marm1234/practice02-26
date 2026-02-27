452615256d730848065b0173528667834214c9ef
marm1234 <159101169+marm1234@users.noreply.github.com>
2026-02-28 05:44:15 +0700

    vuex

---
+++ shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <img 
+        v-if="loading"
+        src="https://i.imgur.com/JfPpwOA.gif">
+        <ul v-else>
+            <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import store from '@/store/index'
+export default {
+    data () {
+        return {
+            loading: false
+        }
+    },
+    computed: {
+        products () {
+            return store.getters.availableProducts
+        }
+    },
+    created() {
+        this.loading = true
+        store.dispatch('fetchProducts')
+        .then(() => this.loading = false)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ shopping-cart/src/store/index.js
@@ -0,0 +1,31 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import shop from '@/api/shop'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        products: [],
+        cart: []
+    },
+    getters: {
+        availableProducts (state, getters) {
+            return state.products.filter(product => product.inventory > 0)
+        }
+    },
+    actions: {
+        fetchProducts({commit}) {
+            return new Promise((resolve, reject) => {
+                shop.getProducts(products => {
+                    commit('setProducts', products)
+                    resolve()
+                })
+            })
+        }
+    },
+    mutations: {
+        setProducts (state, products) {
+            state.products = products
+        }
+    }
+})
